9c1c18c2e2d28740aae83f1d9b55d25c1ce2d1d8
jayxu66 <sgliv8@gmail.com>
2015-06-24 12:12:44 -0700

    Make some changes

--- app/auth/auth.service.js
+++
@@ -1,7 +0,0 @@
-angular.module('weilaSlackApp')
-	.factory('Auth', function($firebaseAuth, FirebaseUrl){
-		var ref = new firebase(FirebaseUrl);
-		var auth = $firebaseAuth(ref);
-
-		return auth;
-	});
