c4beea3c159fe0d2eed6be2a0f2f818c57cd2e6c
jayxu66 <sgliv8@gmail.com>
2015-06-24 16:16:17 -0700

    Added user service for users information storing

---
+++ app/users/usersService.js
@@ -0,0 +1,19 @@
+angular.module('weilaSlackApp')
+	.factory('Users', function($firebaseArray, $firebaseObject, FirebaseUrl){
+		//Added a reference to the users node
+
+		var usersRef = new Firebase(FirebaseUrl+ 'users');
+		var users = $firebaseArray(usersRef);
+
+		var Users = {
+			getProfile: function(uid) {
+				return $firebaseObject(usersRef.child(uid));
+			},
+			getDisplayName: function(uid){
+				return users.$getRecord(uid).displayName;
+			},
+			all: users
+		};
+
+		return Users;
+	});
